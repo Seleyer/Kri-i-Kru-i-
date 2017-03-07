579208aa0899cc02851e1f11cc080ce666d311ec
Seleyer <matek.novi@gmail.com>
2017-03-07 15:21:18 +0100

    KrizicKruzic
    Trippy

---
+++ KrižićKružić.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.btnThree = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnSix = New System.Windows.Forms.Button()
+        Me.btnSeven = New System.Windows.Forms.Button()
+        Me.btnEight = New System.Windows.Forms.Button()
+        Me.btnNine = New System.Windows.Forms.Button()
+        Me.lblDirections = New System.Windows.Forms.Label()
+        Me.btnBegin = New System.Windows.Forms.Button()
+        Me.btnDone = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnOne
+        '
+        Me.btnOne.BackColor = System.Drawing.Color.Transparent
+        Me.btnOne.BackgroundImage = CType(resources.GetObject("btnOne.BackgroundImage"), System.Drawing.Image)
+        Me.btnOne.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnOne.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOne.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btnOne.Location = New System.Drawing.Point(47, 79)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(76, 54)
+        Me.btnOne.TabIndex = 0
+        Me.btnOne.UseVisualStyleBackColor = False
+        '
+        'btnTwo
+        '
+        Me.btnTwo.BackColor = System.Drawing.Color.Transparent
+        Me.btnTwo.BackgroundImage = CType(resources.GetObject("btnTwo.BackgroundImage"), System.Drawing.Image)
+        Me.btnTwo.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnTwo.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTwo.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btnTwo.Location = New System.Drawing.Point(149, 79)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(76, 54)
+        Me.btnTwo.TabIndex = 1
+        Me.btnTwo.UseVisualStyleBackColor = False
+        '
+        'btnThree
+        '
+        Me.btnThree.BackColor = System.Drawing.Color.Transparent
+        Me.btnThree.BackgroundImage = CType(resources.GetObject("btnThree.BackgroundImage"), System.Drawing.Image)
+        Me.btnThree.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnThree.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnThree.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btnThree.Location = New System.Drawing.Point(259, 79)
+        Me.btnThree.Name = "btnThree"
+        Me.btnThree.Size = New System.Drawing.Size(76, 54)
+        Me.btnThree.TabIndex = 2
+        Me.btnThree.UseVisualStyleBackColor = False
+        '
+        'btnFour
+        '
+        Me.btnFour.BackColor = System.Drawing.Color.Transparent
+        Me.btnFour.BackgroundImage = CType(resources.GetObject("btnFour.BackgroundImage"), System.Drawing.Image)
+        Me.btnFour.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnFour.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFour.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btnFour.Location = New System.Drawing.Point(47, 156)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(76, 54)
+        Me.btnFour.TabIndex = 3
+        Me.btnFour.UseVisualStyleBackColor = False
+        '
+        'btnFive
+        '
+        Me.btnFive.BackColor = System.Drawing.Color.Transparent
+        Me.btnFive.BackgroundImage = CType(resources.GetObject("btnFive.BackgroundImage"), System.Drawing.Image)
+        Me.btnFive.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnFive.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFive.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btnFive.Location = New System.Drawing.Point(149, 156)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(76, 54)
+        Me.btnFive.TabIndex = 4
+        Me.btnFive.UseVisualStyleBackColor = False
+        '
+        'btnSix
+        '
+        Me.btnSix.BackColor = System.Drawing.Color.Transparent
+        Me.btnSix.BackgroundImage = CType(resources.GetObject("btnSix.BackgroundImage"), System.Drawing.Image)
+        Me.btnSix.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnSix.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSix.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btnSix.Location = New System.Drawing.Point(259, 156)
+        Me.btnSix.Name = "btnSix"
+        Me.btnSix.Size = New System.Drawing.Size(76, 54)
+        Me.btnSix.TabIndex = 5
+        Me.btnSix.UseVisualStyleBackColor = False
+        '
+        'btnSeven
+        '
+        Me.btnSeven.BackColor = System.Drawing.Color.Transparent
+        Me.btnSeven.BackgroundImage = CType(resources.GetObject("btnSeven.BackgroundImage"), System.Drawing.Image)
+        Me.btnSeven.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnSeven.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSeven.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btnSeven.Location = New System.Drawing.Point(47, 235)
+        Me.btnSeven.Name = "btnSeven"
+        Me.btnSeven.Size = New System.Drawing.Size(76, 54)
+        Me.btnSeven.TabIndex = 6
+        Me.btnSeven.UseVisualStyleBackColor = False
+        '
+        'btnEight
+        '
+        Me.btnEight.BackColor = System.Drawing.Color.Transparent
+        Me.btnEight.BackgroundImage = CType(resources.GetObject("btnEight.BackgroundImage"), System.Drawing.Image)
+        Me.btnEight.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnEight.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEight.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btnEight.Location = New System.Drawing.Point(149, 235)
+        Me.btnEight.Name = "btnEight"
+        Me.btnEight.Size = New System.Drawing.Size(76, 54)
+        Me.btnEight.TabIndex = 7
+        Me.btnEight.UseVisualStyleBackColor = False
+        '
+        'btnNine
+        '
+        Me.btnNine.BackColor = System.Drawing.Color.Transparent
+        Me.btnNine.BackgroundImage = CType(resources.GetObject("btnNine.BackgroundImage"), System.Drawing.Image)
+        Me.btnNine.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnNine.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNine.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btnNine.Location = New System.Drawing.Point(259, 235)
+        Me.btnNine.Name = "btnNine"
+        Me.btnNine.Size = New System.Drawing.Size(76, 54)
+        Me.btnNine.TabIndex = 8
+        Me.btnNine.UseVisualStyleBackColor = False
+        '
+        'lblDirections
+        '
+        Me.lblDirections.AutoSize = True
+        Me.lblDirections.BackColor = System.Drawing.Color.Transparent
+        Me.lblDirections.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.lblDirections.Font = New System.Drawing.Font("Snap ITC", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDirections.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.lblDirections.Location = New System.Drawing.Point(84, 18)
+        Me.lblDirections.Name = "lblDirections"
+        Me.lblDirections.Size = New System.Drawing.Size(222, 42)
+        Me.lblDirections.TabIndex = 9
+        Me.lblDirections.Text = "ZAPOČNI!"
+        '
+        'btnBegin
+        '
+        Me.btnBegin.BackColor = System.Drawing.Color.Transparent
+        Me.btnBegin.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnBegin.Font = New System.Drawing.Font("Mistral", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.btnBegin.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.btnBegin.Location = New System.Drawing.Point(47, 329)
+        Me.btnBegin.Name = "btnBegin"
+        Me.btnBegin.Size = New System.Drawing.Size(127, 40)
+        Me.btnBegin.TabIndex = 10
+        Me.btnBegin.Text = "ZAPOČNI"
+        Me.btnBegin.UseVisualStyleBackColor = False
+        '
+        'btnDone
+        '
+        Me.btnDone.BackColor = System.Drawing.Color.Transparent
+        Me.btnDone.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnDone.Font = New System.Drawing.Font("Mistral", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.btnDone.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.btnDone.Location = New System.Drawing.Point(207, 329)
+        Me.btnDone.Name = "btnDone"
+        Me.btnDone.Size = New System.Drawing.Size(128, 40)
+        Me.btnDone.TabIndex = 11
+        Me.btnDone.Text = "IZLAZ"
+        Me.btnDone.UseVisualStyleBackColor = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(391, 381)
+        Me.Controls.Add(Me.btnDone)
+        Me.Controls.Add(Me.btnBegin)
+        Me.Controls.Add(Me.lblDirections)
+        Me.Controls.Add(Me.btnNine)
+        Me.Controls.Add(Me.btnEight)
+        Me.Controls.Add(Me.btnSeven)
+        Me.Controls.Add(Me.btnSix)
+        Me.Controls.Add(Me.btnFive)
+        Me.Controls.Add(Me.btnFour)
+        Me.Controls.Add(Me.btnThree)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnOne)
+        Me.Name = "frmMain"
+        Me.ShowIcon = False
+        Me.Text = "Tic-Tac-Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents btnThree As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnFive As Button
+    Friend WithEvents btnSix As Button
+    Friend WithEvents btnSeven As Button
+    Friend WithEvents btnEight As Button
+    Friend WithEvents btnNine As Button
+    Friend WithEvents lblDirections As Label
+    Friend WithEvents btnBegin As Button
+    Friend WithEvents btnDone As Button
+End Class
---
+++ KrižićKružić.vb
@@ -0,0 +1,281 @@
+﻿' Class: DWD255 Intermediate Programming with Visual Basic
+' Author: Danielle Yontz
+' Summer 2016
+' Project: Final Class Project
+' September 8, 2016
+
+Public Class frmMain
+    Dim thePlayer As Integer = 1
+    Dim theBoard(2, 2) As Integer
+
+    'begin button initiates gameplay...
+    Private Sub btnBegin_Click(sender As Object, e As EventArgs) Handles btnBegin.Click
+        For row As Integer = 0 To 2
+            For col As Integer = 0 To 2
+                theBoard(row, col) = 0
+            Next col
+        Next row
+
+        btnOne.BackColor = SystemColors.Control
+        btnOne.Text = ""
+        btnOne.Enabled = True
+        btnTwo.BackColor = SystemColors.Control
+        btnTwo.Text = ""
+        btnTwo.Enabled = True
+        btnThree.BackColor = SystemColors.Control
+        btnThree.Text = ""
+        btnThree.Enabled = True
+        btnFour.BackColor = SystemColors.Control
+        btnFour.Text = ""
+        btnFour.Enabled = True
+        btnFive.BackColor = SystemColors.Control
+        btnFive.Text = ""
+        btnFive.Enabled = True
+        btnSix.BackColor = SystemColors.Control
+        btnSix.Text = ""
+        btnSix.Enabled = True
+        btnSeven.BackColor = SystemColors.Control
+        btnSeven.Text = ""
+        btnSeven.Enabled = True
+        btnEight.BackColor = SystemColors.Control
+        btnEight.Text = ""
+        btnEight.Enabled = True
+        btnNine.BackColor = SystemColors.Control
+        btnNine.Text = ""
+        btnNine.Enabled = True
+
+        thePlayer = 1
+        lblDirections.Text = "Player 1 GO!"
+
+    End Sub
+
+    'gives each player a X and an O for each square they click
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+        If (thePlayer = 1) Then
+            btnOne.Text = "X"
+            btnOne.BackColor = Color.LightSeaGreen
+        Else
+            btnOne.Text = "O"
+            btnOne.BackColor = Color.Coral
+        End If
+        theBoard(0, 0) = thePlayer
+        btnOne.Enabled = True
+        EvaluateGame()
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+        If (thePlayer = 1) Then
+            btnTwo.Text = "X"
+            btnTwo.BackColor = Color.LightSeaGreen
+        Else
+            btnTwo.Text = "O"
+            btnTwo.BackColor = Color.Coral
+        End If
+        theBoard(0, 1) = thePlayer
+        btnTwo.Enabled = True
+        EvaluateGame()
+    End Sub
+
+    Private Sub btnThree_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+        If (thePlayer = 1) Then
+            btnThree.Text = "X"
+            btnThree.BackColor = Color.LightSeaGreen
+        Else
+            btnThree.Text = "O"
+            btnThree.BackColor = Color.Coral
+        End If
+        theBoard(0, 2) = thePlayer
+        btnThree.Enabled = True
+        EvaluateGame()
+    End Sub
+
+    Private Sub btnFour_Click(sender As Object, e As EventArgs) Handles btnFour.Click
+        If (thePlayer = 1) Then
+            btnFour.Text = "X"
+            btnFour.BackColor = Color.LightSeaGreen
+        Else
+            btnFour.Text = "O"
+            btnFour.BackColor = Color.Coral
+        End If
+        theBoard(1, 0) = thePlayer
+        btnFour.Enabled = True
+        EvaluateGame()
+    End Sub
+
+    Private Sub btnFive_Click(sender As Object, e As EventArgs) Handles btnFive.Click
+        If (thePlayer = 1) Then
+            btnFive.Text = "X"
+            btnFive.BackColor = Color.LightSeaGreen
+        Else
+            btnFive.Text = "O"
+            btnFive.BackColor = Color.Coral
+        End If
+        theBoard(1, 1) = thePlayer
+        btnFive.Enabled = True
+        EvaluateGame()
+    End Sub
+
+    Private Sub btnSix_Click(sender As Object, e As EventArgs) Handles btnSix.Click
+        If (thePlayer = 1) Then
+            btnSix.Text = "X"
+            btnSix.BackColor = Color.LightSeaGreen
+        Else
+            btnSix.Text = "O"
+            btnSix.BackColor = Color.Coral
+        End If
+        theBoard(1, 2) = thePlayer
+        btnSix.Enabled = True
+        EvaluateGame()
+    End Sub
+
+    Private Sub btnSeven_Click(sender As Object, e As EventArgs) Handles btnSeven.Click
+        If (thePlayer = 1) Then
+            btnSeven.Text = "X"
+            btnSeven.BackColor = Color.LightSeaGreen
+        Else
+            btnSeven.Text = "O"
+            btnSeven.BackColor = Color.Coral
+        End If
+        theBoard(2, 0) = thePlayer
+        btnSeven.Enabled = True
+        EvaluateGame()
+    End Sub
+
+    Private Sub btnEight_Click(sender As Object, e As EventArgs) Handles btnEight.Click
+        If (thePlayer = 1) Then
+            btnEight.Text = "X"
+            btnEight.BackColor = Color.LightSeaGreen
+        Else
+            btnEight.Text = "O"
+            btnEight.BackColor = Color.Coral
+        End If
+        theBoard(2, 1) = thePlayer
+        btnEight.Enabled = True
+        EvaluateGame()
+    End Sub
+
+    Private Sub btnNine_Click(sender As Object, e As EventArgs) Handles btnNine.Click
+        If (thePlayer = 1) Then
+            btnNine.Text = "X"
+            btnNine.BackColor = Color.LightSeaGreen
+        Else
+            btnNine.Text = "O"
+            btnNine.BackColor = Color.Coral
+        End If
+        theBoard(2, 2) = thePlayer
+        btnNine.Enabled = True
+        EvaluateGame()
+
+    End Sub
+
+    'checks for a winner or a tie game
+    Private Sub EvaluateGame()
+        If IsWinner() Then
+            lblDirections.Text = "Player " & CStr(thePlayer) & " Wins!"
+            btnOne.Enabled = False
+            btnTwo.Enabled = False
+            btnThree.Enabled = False
+            btnFour.Enabled = False
+            btnFive.Enabled = False
+            btnSix.Enabled = False
+            btnSeven.Enabled = False
+            btnEight.Enabled = False
+            btnNine.Enabled = False
+        Else
+            If (lblDirections.Text <> "Tie Game!") Then
+                thePlayer = thePlayer + 1
+                If (thePlayer > 2) Then
+                    thePlayer = 1
+                End If
+                lblDirections.Text = "Player " & CStr(thePlayer) & " GO!"
+            End If
+        End If
+    End Sub
+
+    Private Function IsWinner()
+        Dim Result As Boolean = False
+
+
+        'checks all the rows to see if there is a winner
+        If ((theBoard(0, 0) > 0) And (theBoard(0, 1) > 0) And (theBoard(0, 2) > 0)) Then
+            If ((theBoard(0, 0) = theBoard(0, 1)) And (theBoard(0, 1) = theBoard(0, 2))) Then
+                Result = True
+            End If
+        End If
+
+        If Result = False Then
+            If ((theBoard(1, 0) > 0) And (theBoard(1, 1) > 0) And (theBoard(1, 2) > 0)) Then
+                If ((theBoard(1, 0) = theBoard(1, 1)) And (theBoard(1, 1) = theBoard(1, 2))) Then
+                    Result = True
+                End If
+            End If
+        End If
+
+        If Result = False Then
+            If ((theBoard(2, 0) > 0) And (theBoard(2, 1) > 0) And (theBoard(2, 2) > 0)) Then
+                If ((theBoard(2, 0) = theBoard(2, 1)) And (theBoard(2, 1) = theBoard(2, 2))) Then
+                    Result = True
+                End If
+            End If
+        End If
+
+
+        'checks all the columns to see if there is a winner
+        If Result = False Then
+            If ((theBoard(0, 0) > 0) And (theBoard(1, 0) > 0) And (theBoard(2, 0) > 0)) Then
+                If ((theBoard(0, 0) = theBoard(1, 0)) And (theBoard(1, 0) = theBoard(2, 0))) Then
+                    Result = True
+                End If
+            End If
+        End If
+
+
+        If Result = False Then
+            If ((theBoard(0, 1) > 0) And (theBoard(1, 1) > 0) And (theBoard(2, 1) > 0)) Then
+                If ((theBoard(0, 1) = theBoard(1, 1)) And (theBoard(1, 1) = theBoard(2, 1))) Then
+                    Result = True
+                End If
+            End If
+        End If
+
+        If Result = False Then
+            If ((theBoard(0, 2) > 0) And (theBoard(1, 2) > 0) And (theBoard(2, 2) > 0)) Then
+                If ((theBoard(0, 2) = theBoard(1, 2)) And (theBoard(1, 2) = theBoard(2, 2))) Then
+                    Result = True
+                End If
+            End If
+        End If
+
+
+        'checks for the two diagonal winners
+        If Result = False Then
+            If ((theBoard(0, 0) > 0) And (theBoard(1, 1) > 0) And (theBoard(2, 2) > 0)) Then
+                If ((theBoard(0, 0) = theBoard(1, 1)) And (theBoard(1, 1) = theBoard(2, 2))) Then
+                    Result = True
+                End If
+            End If
+        End If
+
+        If Result = False Then
+            If ((theBoard(2, 0) > 0) And (theBoard(1, 1) > 0) And (theBoard(0, 2) > 0)) Then
+                If ((theBoard(2, 0) = theBoard(1, 1)) And (theBoard(1, 1) = theBoard(0, 2))) Then
+                    Result = True
+                End If
+            End If
+        End If
+
+        'checks for a tie game
+        If Result = False Then
+            If ((theBoard(0, 0) > 0) And (theBoard(0, 1) > 0) And (theBoard(0, 2) > 0) And (theBoard(1, 0) > 0) And (theBoard(1, 1) > 0) And (theBoard(1, 2) > 0) And (theBoard(2, 0) > 0) And (theBoard(2, 1) > 0) And (theBoard(2, 2) > 0)) Then
+                lblDirections.Text = "Tie Game!"
+            End If
+        End If
+
+        Return Result
+    End Function
+
+    'Done closes the window
+    Private Sub btnDone_Click(sender As Object, e As EventArgs) Handles btnDone.Click
+        Me.Close()
+    End Sub
+End Class
